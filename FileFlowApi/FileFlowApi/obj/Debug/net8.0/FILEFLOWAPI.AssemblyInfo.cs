3fbcce5b1c542c50ba352220be559a4e6c722596
Hadasa <h4124196@gmail.com>
2025-05-28 23:55:43 +0300

    dockerfile

--- FileFlowApi/FileFlowApi/obj/Debug/net8.0/FILEFLOWAPI.AssemblyInfo.cs
+++ FileFlowApi/FileFlowApi/obj/Debug/net8.0/FILEFLOWAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FILEFLOWAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b422c245f13bd0cc277438a0e20ecdf1bae93f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5790715d3e6810326c6fa2ac40c03e2465aa3586")]
 [assembly: System.Reflection.AssemblyProductAttribute("FILEFLOWAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FILEFLOWAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FileFlowApi/service/obj/Debug/net8.0/service.AssemblyInfo.cs
+++ FileFlowApi/service/obj/Debug/net8.0/service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2890b26d8fe56526d9db3ecdf21cbc841d7031")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5790715d3e6810326c6fa2ac40c03e2465aa3586")]
 [assembly: System.Reflection.AssemblyProductAttribute("service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
